0116a3e478a4b64b2726e4cec5b41b62f2e93105
CHerbers <cherbers@gmx.net>
2021-08-30 12:58:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HighSenso
+ An app to personally analyse if you are an HSP
---
+++ app/src/main/java/name/herbers/android/highsenso/MainActivity.kt
@@ -0,0 +1,11 @@
+package name.herbers.android.highsenso
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
